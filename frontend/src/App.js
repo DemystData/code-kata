d66879c20ccbfbbbdc743a13fc10aa97c4fe37aa
vairavan2001 <136603137+vairavan2001@users.noreply.github.com>
2023-08-31 11:11:37 +0000

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <>
     <BrowserRouter>
     <Routes>
-      <Route path={'/home'} element={<Home/>}></Route>
+      <Route path={'/'} element={<Home/>}></Route>
       <Route path={'/form'} element={<ApplicationForm/>}></Route>
     </Routes>
     </BrowserRouter>
