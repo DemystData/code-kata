3b32a2d6c7137e640eae6b6c4a9e60a82a434730
Bhawesh <bhaweshverma50@gmail.com>
2023-09-01 09:19:33 +0530

    App Welcome screen

---
+++ loan-app/app/layout.tsx
@@ -0,0 +1,18 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Loanr | Welcome to the Loaner App',
+  description: 'Getting loans is hard. We help you get it easy.',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ loan-app/app/page.tsx
@@ -0,0 +1,22 @@
+import { Button } from '@/components/ui/button';
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col p-12 bg-gradient-to-b from-slate-900 to-slate-800 justify-center">
+      <div className="text-white w-full lg:w-[50%]">
+        <h1 className="text-5xl md:text-6xl font-semibold leading-tight mb-6">
+          <div className="mb-2">Hey Guest,</div>Welcome to <span className="gradient-text">Loanr!</span>
+        </h1>
+        <p className="text-xl font-normal md:text-3xl mb-6 text-slate-300">
+          Say goodbye to loan application rejections.
+        </p>
+        <p className="text-lg md:text-xl mb-2 font-light text-gray-400">
+          Loanr empowers you with smart insights to increase your loan approval chances. Our advanced assessment
+          analyzes your business and provides a tailored success prediction for your loan application.
+        </p>
+        <Button className="mt-6" size="lg">
+          Get Started
+        </Button>
+      </div>
+    </main>
+  );
+}
