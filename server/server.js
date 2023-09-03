ca1170fd02e9c9c524368d9c7945cd5bc6d6eefe
00rj00 <harshitvadlamani424@gmail.com>
2023-09-03 19:06:30 +0530

    Final

--- server/server.js
+++ server/server.js
@@ -3,37 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const pgp = require('pg-promise')();
-const dbConfig = {
-  // Your database configuration options here
-  // For example:
-  host: 'localhost',
-  port: 5432,
-  database: 'your_database_name',
-  user: 'your_username',
-  password: 'your_password',
-};
-const db = pgp(dbConfig);
-
-// Create the database table (assuming it doesn't exist yet)
-const createTableQuery = `
-  CREATE TABLE IF NOT EXISTS balance_sheets (
-    id SERIAL PRIMARY KEY,
-    business_name TEXT NOT NULL,
-    year INT NOT NULL,
-    month INT NOT NULL,
-    profit_or_loss NUMERIC NOT NULL,
-    assets_value NUMERIC NOT NULL
-  )
-`;
-
-db.none(createTableQuery)
-  .then(() => {
-    console.log('Table "balance_sheets" created successfully');
-  })
-  .catch((error) => {
-    console.error('Error creating table:', error);
-  });
-
+const db = pgp('postgresql://postgres:<password>@localhost:5432/balance_sheets');
 
 // Configure CORS settings
 const corsOptions = {
