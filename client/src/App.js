67e612641447cb28442f27c29ab0f0866dad56ab
00rj00 <harshitvadlamani424@gmail.com>
2023-09-03 17:50:31 +0530

    Done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/Navbar';
+import PreAssessmentForm from './components/PreAssessmentForm';
+import './index.css';
+
+
 function App() {
   return (
-    <div className="App">
-      Nice
-    </div>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<PreAssessmentForm />} />
+      </Routes>
+    </Router>
   );
 }
+
 
 export default App;
